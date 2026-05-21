49ebaa3ef799e360d89f6843267149d766c10ba3
Akshay Kumar <your-email@example.com>
2026-05-21 16:52:30 +0530

    update gtag

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,23 @@ export default function RootLayout({ children }) {
       {/* ✅ ADD THIS BLOCK */}
       <head>
         <base href="/" />
+        <head>
+          {/* Google tag (gtag.js) */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-R6ZG8XRESL"
+            strategy="afterInteractive"
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R6ZG8XRESL');
+          `}
+          </Script>
+        </head>
       </head>
 
       <body>
