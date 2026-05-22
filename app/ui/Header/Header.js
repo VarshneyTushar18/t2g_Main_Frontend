c632508036ba43b00ba883f96e92d81b747dc098
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-05-22 13:42:03 +0530

    show lifeat2g

--- app/ui/Header/Header.js
+++ app/ui/Header/Header.js
@@ -50,7 +50,7 @@ export default function Header() {
                            <li><Link href="csr-initiatives">CSR Initiatives</Link></li>
                            <li><Link href="5years-milemakers">5 Years Milemakers</Link></li>
                            <li><Link href="our-team">Our Team</Link></li>
-                           {/* <li><Link href="life-at-tech2globe">Life at Tech2globe</Link></li> */}
+                           <li><Link href="life-at-tech2globe">Life at Tech2globe</Link></li>
                            <li><Link href="career">Career</Link></li>
                         </ul>
                      </li>
