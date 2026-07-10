64b5146a8d1b13a5965c9576ae943193f52abacf
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-07-10 16:41:12 +0530

    changd script

--- app/amazon-ads/page.js
+++ app/amazon-ads/page.js
@@ -103,18 +103,27 @@ export default function AmazonAds() {
     return (
         <>
             <Script
-                src="https://www.googletagmanager.com/gtag/js?id=AW-972611168"
+                src="https://www.googletagmanager.com/ns.html?id=GTM-NNKV63RF"
                 strategy="afterInteractive"
             />
 
-            <Script id="google-ads-contact" strategy="afterInteractive">
-                {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-972611168');
-        `}
-            </Script>
+            <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NNKV63RF');
+          `}
+        </Script>
 
             <header className="d2c_navbar sticky-top">
                 <div className="container">
