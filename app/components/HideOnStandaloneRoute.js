1d451930d9fc558da1969127d8f736a47614cee2
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-07-10 19:19:44 +0530

    updates

--- app/components/HideOnStandaloneRoute.js
+++ app/components/HideOnStandaloneRoute.js
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 
 const STANDALONE_ROUTES = [
   "/amazon-ads",
+  "/amazon-consulting-australia",
   "/shopify-development-company/questionnaire",
 ];
 
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import BootstrapClient from "./utilities/BootstrapClient";
 import LightboxInitializer from "./utilities/LightboxInitializer";
@@ -12,10 +12,10 @@ import { fixRelativeLinks } from "@/app/utilities/fixRelativeLinks";
 import FixLinksWrapper from "@/app/utilities/FixLinksWrapper";
 import Script from "next/script";
 
-const plusJakarta = Plus_Jakarta_Sans({
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
-  variable: "--font-plus-jakarta",
+  variable: "--font-poppins",
   display: "swap",
 });
 
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={plusJakarta.variable}>
+    <html lang="en" className={poppins.variable}>
       {/* ✅ ADD THIS BLOCK */}
       <head>
         <base href="/" />
