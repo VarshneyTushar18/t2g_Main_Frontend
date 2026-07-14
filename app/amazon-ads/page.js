126dbdf6fb8951983121db38901b24d7ef949375
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-07-14 17:38:25 +0530

    update meta

--- app/amazon-ads/page.js
+++ app/amazon-ads/page.js
@@ -102,28 +102,6 @@ export default function AmazonAds() {
 
     return (
         <>
-            <Script
-                src="https://www.googletagmanager.com/ns.html?id=GTM-NNKV63RF"
-                strategy="afterInteractive"
-            />
-
-            <Script id="gtm" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event:'gtm.js'
-              });
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NNKV63RF');
-          `}
-        </Script>
 
             <header className="d2c_navbar sticky-top">
                 <div className="container">
